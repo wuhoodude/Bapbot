0a12b8c5ee409ff1a5fddbd86700ce6b436c6e4e
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2018-04-14 23:55:04 -0600

    fix bap

--- plugins/md.js
+++ plugins/md.js
@@ -141,13 +141,13 @@ let commands = {
 	bap: function (target, room, user) {
 		if (!(room instanceof Users.User) && !user.hasRank(room, '+')) return;
 		if (!target) return this.say("/wall BAP");
-		if (target === '~') return this.say ("/wall ~~BAP~~");
-		if (target ==='*') return this.say ("/wall **BAP**");
-		if (target === '_') return this.say ("/wall __BAP__");
-		if (target ==='^') return this.say ("/wall ^^BAP^^");
-		if (target ==="'\'") return this.say ("/wall \\BAP\\"); //Fix command to format subscript bap
-		if (target ==='`') return this.say ("/wall ``BAP``");
-		if (target === '>') return this.say ("/wall >BAP");
+		if (target === '~') return this.say("/wall ~~BAP~~");
+		if (target ==='*') return this.say("/wall **BAP**");
+		if (target === '_') return this.say("/wall __BAP__");
+		if (target ==='^') return this.say("/wall ^^BAP^^");
+		if (target ==='\\') return this.say("/wall \\BAP\\\\"); //Fix command to format subscript bap
+		if (target ==='`') return this.say("/wall ``BAP``");
+		if (target === '>') return this.say("/wall >BAP");
 		if (target === 'me') return this.say("/me BAPS");
 		if (target === 'spoil') return this.say("spoiler:BAP");
 		if (target === 'link') return this.say("[[BAP]]");
